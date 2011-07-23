7b6d8107dea77003d91cfe4a6b6bc358ac1a3531
Steven K <tseven@gmail.com>
2011-07-23 12:47:37 -0700

    Allow example to be executed as php, added run time timer

--- example.php
+++ example.php
@@ -1,5 +1,7 @@
+<?
 ##ezScrabble by s0up(mike@haxt.net)##
-
+include 'ezScrabble.php';
+$start = getTime();
 $scrabble = new Scrabble("wordlist");
 
 $letters = "adoomfcs";
@@ -10,3 +12,11 @@ $scrabble->set_var("prefix", $prefix);
 $scrabble->set_var("suffix", $suffix);
 
 print_r($scrabble->get_words($letters));
+
+$end = getTime(); 
+echo "Time taken = " . number_format(($end - $start), 2) . " secs\n";
+
+function getTime(){ 
+  $a = explode (' ',microtime()); 
+  return(double) $a[0] + $a[1]; 
+} 
